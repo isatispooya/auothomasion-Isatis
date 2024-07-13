f9500f6a7d5c3e1f18961fec1ab0fc04750850db
rahele <rahelemomenyfar0113@gmail.com>
2024-07-13 13:58:47 +0330

    .

--- auothomasion-isatis/authentication/urls.py
+++ auothomasion-isatis/authentication/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 from .views import CaptchaViewest, OtpViewest
 
+urlpatterns = [
+    path('captcha/',CaptchaViewest.as_view(),name = 'captcha'),
+    path('otp/',OtpViewest.as_view(),name = 'otp'),
+
+]
